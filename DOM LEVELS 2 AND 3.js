0670e9f0f729107493cf602659cfc4aa809320d0
fallenjlz <32124530+fallenjlz@users.noreply.github.com>
2017-12-22 15:38:51 +0800

    Update DOM LEVELS 2 AND 3.js

--- DOM LEVELS 2 AND 3.js
+++ DOM LEVELS 2 AND 3.js
@@ -17,3 +17,27 @@ for (i=0, len=myDiv.style.length; i < len; i++){
     value = myDiv.style.getPropertyCSSValue(prop);
     alert(prop + " : " + value.cssText + " (" + value.cssValueType +")");
 }
+
+
+var myDiv = document.getElementById("myDiv");
+		var computedStyle = myDiv.currentStyle;
+		var computedStyle = document.defaultView.getComputedStyle(myDiv, null);
+
+		alert(computedStyle.backgroundColor);
+		alert(computedStyle.width);
+		alert(computedStyle.height);
+		alert(computedStyle.border);
+
+		var sheet = document.styleSheets[0];
+		var rules = sheet.cssRules || sheet.rules; //get rules list
+		var rule = rules[0]; //get first rule
+		alert(rule.selectorText);
+		alert(rule.style.cssText);
+		alert(rule.style.backgroundColor);
+		alert(rule.style.width);
+		alert(rule.style.height);
+
+		var sheet = document.styleSheets[0];
+		var rules = sheet.cssRules || sheet.rules;
+		var rule = rules[0];
+		rule.style.backgroundColor = "red";
